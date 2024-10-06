a02003196232fb5e8453c8492181f4b858f2217f
Dinesh Gavai <gavaidinesh26@gmail.com>
2024-10-06 16:30:49 +0530

    seats fix bug

--- movies/views.py
+++ movies/views.py
@@ -12,6 +12,16 @@ def preferences(request):
     return render(request, "preferences.html")
 
 def seats(request):
+    show_date = request.GET.get('show_date')
+    show_time = request.GET.get('show_time')
+    language = request.GET.get('language')
+    location = request.GET.get('location')
+    print(show_date)
+    print(show_time)
+    print(language)
+    print(location)
+    
+    
     return render(request,'seats.html')
 
 def payment(request):
@@ -21,29 +31,29 @@ def ticket(request):
     return render(request,'ticket.html')
 
 def get_movies(request):
-    movies = Movie.objects.all().values()  # Get all movie records as dictionaries
+    movies = Movie.objects.all().values()  
     formatted_movies = []
 
     for movie in movies:
-        # Convert string representations of lists into actual lists
+        
         formatted_movie = {
             "id": movie["id"],
             "poster": movie["poster"],
-            "directors": json.loads(movie["directors"]),  # Convert string to list
+            "directors": json.loads(movie["directors"]), 
             "name": movie["name"],
-            "cast": json.loads(movie["cast"]),  # Convert string to list
+            "cast": json.loads(movie["cast"]), 
             "rating": movie["rating"],
             "ratingCategory": movie["ratingCategory"],
-            "genre": json.loads(movie["genre"]),  # Convert string to list
+            "genre": json.loads(movie["genre"]), 
             "description": movie["description"],
             "availableFrom": movie["availableFrom"],
             "availableTo": movie["availableTo"],
-            "timings": json.loads(movie["timings"]),  # Convert string to list
-            "availableLocations": json.loads(movie["availableLocations"]),  # Convert string to list
-            "availableScreens": json.loads(movie["availableScreens"]),  # Convert string to list
+            "timings": json.loads(movie["timings"]), 
+            "availableLocations": json.loads(movie["availableLocations"]), 
+            "availableScreens": json.loads(movie["availableScreens"]), 
             "releaseDate": movie["releaseDate"],
             "duration": movie["duration"],
-            "language": json.loads(movie["language"]),  # Convert string to list
+            "language": json.loads(movie["language"]), 
             "trailer": movie["trailer"],
             "price": movie["price"],
             "dimensional": movie["dimensional"],
